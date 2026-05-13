67f3ab8046523ea44046449a22297094b9da5cf6
Furqon <furqon@example.com>
2026-05-13 12:22:05 +0700

    apaja

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -38,6 +38,7 @@
     <a href="{{ route('catalog.index', ['search' => 'Pashmina']) }}" class="btn {{ str_contains($currentSearch, 'pashmina') ? 'btn-maroon border-0 text-white' : 'btn-white border text-muted' }} rounded-pill px-4 py-2 me-2 shadow-sm fw-semibold" style="font-size: 0.9rem;">Pashmina</a>
     <a href="{{ route('catalog.index', ['search' => 'Bergo']) }}" class="btn {{ str_contains($currentSearch, 'bergo') ? 'btn-maroon border-0 text-white' : 'btn-white border text-muted' }} rounded-pill px-4 py-2 me-2 shadow-sm fw-semibold" style="font-size: 0.9rem;">Bergo</a>
     <a href="{{ route('catalog.index', ['search' => 'Ciput']) }}" class="btn {{ str_contains($currentSearch, 'ciput') ? 'btn-maroon border-0 text-white' : 'btn-white border text-muted' }} rounded-pill px-4 py-2 me-2 shadow-sm fw-semibold" style="font-size: 0.9rem;">Ciput</a>
+    <a href="{{ route('catalog.index', ['search' => 'Viscose']) }}" class="btn {{ str_contains($currentSearch, 'bergo') ? 'btn-maroon border-0 text-white' : 'btn-white border text-muted' }} rounded-pill px-4 py-2 me-2 shadow-sm fw-semibold" style="font-size: 0.9rem;">Viscose</a>
 </div>
 
 {{-- Section Title --}}
